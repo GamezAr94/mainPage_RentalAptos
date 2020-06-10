08cb0ba91d211f146deeb9bbcd2d9a2cff54a0d1
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-06-10 09:03:01 -0700

    finalizando menu desplegable
    mostrando el menu desplegable cuando la hamburguesa el clickeada

--- scripts/menu_btn.js
+++ scripts/menu_btn.js
@@ -1,5 +1,12 @@
+/*
+    funcion para que el menu cambie de clase y se muestr o se oculte cada que sea clickeado
+*/
 const btn = document.getElementById('menu-hamburger');
+const menu = document.getElementById('menu');
 function onClickMenu(){
     btn.classList.toggle("burger-close");
     btn.classList.toggle("burger");
+
+    menu.classList.toggle("show");
+    menu.classList.toggle("hide");
 }
