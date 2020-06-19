17230761816dc6f79488c390e737e7b77c482886
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-06-18 21:14:56 -0700

    modificando sql
    modificando el sql para que los apartamentos sin contrato de tenant puedan aparecer automaticamente en la pagina inicial como contratar NOW

--- includes/variablesSQL.inc.php
+++ includes/variablesSQL.inc.php
@@ -1,4 +1,5 @@
 <?php
+//where the last room contract and the last apartament contract and the last apartament contract is greather than todays date and room id is equal to...
 $displayInfoApartments = "SELECT *
                         FROM apartaments
                         right join room
@@ -13,14 +14,17 @@ $displayInfoApartments = "SELECT *
                                         group by room_fk) AND aptocontract.ac_endD in (
                                                                 select MAX(ac_endD)
                                                                 FROM aptocontract
-                                                                group by apts_fk) AND aptocontract.ac_endD > CAST(CURRENT_TIMESTAMP AS DATE) AND room.room_id = ?";
-    $allApartmentsSQL = "SELECT *
+                                                                group by apts_fk) AND aptocontract.ac_endD > curdate() AND room.room_id = ?";
+
+//this sql display the current date, but CURDATE() only works in MySQL++++++++++++++++++++++++++++++++++
+//where the last contract of the room and the contract of the apartment is current, and that last contract is greather than todays date, or the contract room is empty
+$allApartmentsSQL = "SELECT *
                 FROM apartaments
                 Inner join room
                 on room.apts_fk = apartaments.apts_id
                 INNER JOIN aptocontract
                 on aptocontract.apts_fk = apartaments.apts_id
-                inner join room_users
+                left join room_users
                 on room_users.room_fk = room.room_id
                 where room_users.ru_endD in (
                                 select MAX(ru_endD)
@@ -28,17 +32,18 @@ $displayInfoApartments = "SELECT *
                                 group by room_fk) AND aptocontract.ac_endD in (
                                                         select MAX(ac_endD)
                                                         FROM aptocontract
-                                                        group by apts_fk) AND aptocontract.ac_endD > CAST(CURRENT_TIMESTAMP AS DATE)
-                order by room.room_price asc;";
+                                                        group by apts_fk) AND aptocontract.ac_endD > curdate() OR room_users.ru_startD is NULL
+                                                        order by room_users.ru_endD asc;";
 
-//this sql display the current date, but CURDATE() only works in MySQL
+//this sql display the current date, but CURDATE() only works in MySQL++++++++++++++++++++++++++++++++++
+//where the last contract to expire by each room, and the contract of the apartment is current and the last contract of the apartment is greather than todays date or the contract in the room is null
     $thisMonthApartments = "SELECT *
     FROM apartaments
     Inner join room
     on room.apts_fk = apartaments.apts_id
     INNER JOIN aptocontract
     on aptocontract.apts_fk = apartaments.apts_id
-    inner join room_users
+    left join room_users
     on room_users.room_fk = room.room_id
     where room_users.ru_endD in (
                     select MAX(ru_endD)
@@ -46,5 +51,5 @@ $displayInfoApartments = "SELECT *
                     group by room_fk) AND aptocontract.ac_endD in (
                                             select MAX(ac_endD)
                                             FROM aptocontract
-                                            group by apts_fk) AND aptocontract.ac_endD > CAST(CURRENT_TIMESTAMP AS DATE) AND room_users.ru_endD < curdate()
+                                            group by apts_fk) AND aptocontract.ac_endD > curdate() AND room_users.ru_endD < curdate() OR room_users.ru_startD is NULL
                     order by room.room_price asc;";
