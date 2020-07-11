19d9d68e0c231842697b408ab4354cd81f92927b
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-07-10 22:23:53 -0700

    starting the user page design
    adding a table for the summarize from last month

--- user.php
+++ user.php
@@ -1,10 +1,57 @@
 <?php
     require "header.php";
 ?>
+    <div id="mainUserContent">
+        <div class="userSalut"> <p>Welcolme again Arturo Gamez.</p></div>
+        <div class="content">
+            <div class="summarize">
+                <div>
+                    <h4>Summarize</h4>
+                    <p>January 2020</p>
+                </div>
+                <table>
+                    <tr>
+                        <th>Rent this month:</th>
+                        <th>$1200</th>
+                    </tr>
+                    <tr>
+                        <th>BCHydro:</th>
+                        <th>$40</th>
+                    </tr>
+                    <tr>
+                        <th>Internet:</th>
+                        <th>$90</th>
+                    </tr>
+                    <tr>
+                        <th>Early payment:</th>
+                        <th>-$50</th>
+                    </tr>
+                    <tr>
+                        <th>Total:</th>
+                        <th>$1280</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+        <p>HOLA</p>
+    </div>
+
+
+<!--
     <h1>HOLAAAA</h1>
     <main>
     <?php
     //checar si la secion esta iniciada 
+        /*
         if(isset($_SESSION['userId'])){
             echo '<p>You are logged in!!!!!!</p>';
             echo $_SESSION['userId'];
@@ -12,9 +59,10 @@
         }else{
             header("Location: index.php?error=session-not-started");
         }
+        */
     ?>
     </main>
-
+    -->
 <?php
     require "footer.php";
 ?>
