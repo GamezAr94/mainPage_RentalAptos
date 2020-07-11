67669da1dff6abc9468e9308d3bb5c7a8dacf302
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-07-11 12:59:53 -0700

    finishing summarize from the user page

--- user.php
+++ user.php
@@ -2,16 +2,17 @@
     require "header.php";
 ?>
     <div id="mainUserContent">
-        <div class="userSalut"> <p>Welcolme again Arturo Gamez.</p></div>
+        <div class="userSalut"><i>Icon</i> <p>Welcolme&nbsp;</p><p id="userName">Arturo Gamez.</p></div>
         <div class="content">
             <div class="summarize">
-                <div>
+                <div class="title">
                     <h4>Summarize</h4>
-                    <p>January 2020</p>
+                    <p class="label">Last payment:</p>
+                    <p class="lastMonth">15, January 2020</p>
                 </div>
                 <table>
                     <tr>
-                        <th>Rent this month:</th>
+                        <th>Rent:</th>
                         <th>$1200</th>
                     </tr>
                     <tr>
@@ -32,17 +33,37 @@
                     </tr>
                 </table>
             </div>
+            <div class="generalInfo">
+            <div class="summarize">
+                <div class="title">
+                    <h4>Information</h4>
+                </div>
+                <table>
+                    <tr>
+                        <th>Rent:</th>
+                        <th>$1200</th>
+                    </tr>
+                    <tr>
+                        <th>BCHydro:</th>
+                        <th>$40</th>
+                    </tr>
+                    <tr>
+                        <th>Internet:</th>
+                        <th>$90</th>
+                    </tr>
+                    <tr>
+                        <th>Early payment:</th>
+                        <th>-$50</th>
+                    </tr>
+                    <tr>
+                        <th>Total:</th>
+                        <th>$1280</th>
+                    </tr>
+                </table>
+            </div>
+            </div>
         </div>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
-        <p>HOLA</p>
+        
     </div>
 
 
