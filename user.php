0db681a7e333744c62236cc045673cb7cb21c056
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-07-11 14:57:05 -0700

    Finishing the landing user page
    finishing the design of the account user page

--- user.php
+++ user.php
@@ -7,8 +7,8 @@
             <div class="summarize">
                 <div class="title">
                     <h4>Summarize</h4>
-                    <p class="label">Last payment:</p>
-                    <p class="lastMonth">15, January 2020</p>
+                    <p class="label">Next payment:</p>
+                    <p class="lastMonth">31, January 2020</p>
                 </div>
                 <table>
                     <tr>
@@ -33,31 +33,31 @@
                     </tr>
                 </table>
             </div>
-            <div class="generalInfo">
-            <div class="summarize">
+            <div class="summarize information">
                 <div class="title">
                     <h4>Information</h4>
+                    <p class="label">Tenant Information</p>
                 </div>
                 <table>
                     <tr>
-                        <th>Rent:</th>
-                        <th>$1200</th>
+                        <th>Entry Date:</th>
+                        <th>Jan 1, 2020</th>
                     </tr>
                     <tr>
-                        <th>BCHydro:</th>
-                        <th>$40</th>
+                        <th>End of Contract:</th>
+                        <th>Jun 30, 2020</th>
                     </tr>
                     <tr>
-                        <th>Internet:</th>
-                        <th>$90</th>
+                        <th>Next Payment Day:</th>
+                        <th>Jan 31, 2020</th>
                     </tr>
                     <tr>
-                        <th>Early payment:</th>
-                        <th>-$50</th>
+                        <th>Damage Deposit:</th>
+                        <th>$600</th>
                     </tr>
                     <tr>
-                        <th>Total:</th>
-                        <th>$1280</th>
+                        <th>Early Payment:</th>
+                        <th>25</th>
                     </tr>
                 </table>
             </div>
